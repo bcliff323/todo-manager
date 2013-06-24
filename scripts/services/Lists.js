f07c120c5e1665dbabe5ff1f57833d866ba381c6
Brian Clifford <bcliff323@gmail.com>
2013-06-23 22:25:16 -0700

    Added list service

---
+++ scripts/services/Lists.js
@@ -0,0 +1,45 @@
+define([
+	'services/services'
+	],
+	function(services) {
+		services.factory('Lists',
+			function(){
+				var Lists = {};
+
+				Lists.query = function() {
+					return [
+						{
+							list: 'Home',
+							details: [
+								{
+									item: 'placeholder',
+									statusCode: 1
+								}
+							]
+						},
+						{
+							list: 'About',
+							details: [
+								{
+									item: 'placeholder2',
+									statusCode: 0
+								}
+							]
+						},
+						{
+							list: 'Another',
+							details: [
+								{
+									item: 'placeholder3',
+									statusCode: 2
+								}
+							]
+						}
+					];
+				}
+
+				return Lists;
+			}
+		);
+	}
+);
